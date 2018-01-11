eeb5650188f26727d45eeedb8f5fa3ba2b805b39
Mauro Marssola <mauro.marssola@visie.com.br>
2018-01-11 17:04:06 -0200

    Start/Stop interface

--- SystemSettings/network.cpp
+++ SystemSettings/network.cpp
@@ -1,5 +1,8 @@
 #include "network.h"
 #include <QDebug>
+#include <QFile>
+#include <QProcess>
+#include <QTextStream>
 #include <iostream>
 
 Network::Network()
@@ -31,4 +34,86 @@ bool Network::setAddress(QJsonObject &data)
     if (query)
         getAddress(m_fields, QString(""));
     return query;
+}
+
+bool Network::writeInterfacesConf()
+{
+    QFile write_interfaces(m_interfaces_file);
+    if (!write_interfaces.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate))
+    {
+        setError(QString("writeInterfacesConf error: %1").arg(write_interfaces.errorString()));
+        return false;
+    }
+    else
+    {
+        QTextStream out(&write_interfaces);
+        out << m_content_interfaces_file << '\n';
+
+        QStringList interfaces;
+        for (const auto &item : m_list_network_address)
+        {
+            QString interface = item.value("interface").toString();
+            if (interface != "")
+            {
+                interfaces.append(item.value("interface").toString());
+
+                out << "auto " << interface << '\n';
+                if (item.value("method").toString() == "manual")
+                {
+                    out << "iface " << interface << (QString(item.value("ip_version").toString()).toInt() == 4? " inet" : " inet6") << " static\n";
+                    out << "\taddress " << item.value("address").toString() << '\n';
+                    out << "\tnetmask " << item.value("mask").toString() << '\n';
+                    out << "\tgateway " << item.value("gateway").toString() << '\n';
+                    out << '\n';
+                }
+                else
+                {
+                    out << "iface " << interface << " inet dhcp\n";
+                    out << '\n';
+                }
+            }
+        }
+        for (const auto &item : m_default_interfaces)
+        {
+            if (!interfaces.contains(item))
+            {
+                out << "auto " << item << '\n';
+                out << "iface " << item << " inet dhcp\n\n";
+            }
+        }
+        write_interfaces.close();
+    }
+    return true;
+}
+
+void Network::startInterface(QString interface)
+{
+    if (interface.isNull() || interface == "")
+    {
+        setError(QString("startInterface error: Interface is null"));
+        return;
+    }
+    QString command = QString("/bin/bash \"ifup %1\"").arg(interface);
+    QProcess start_interface;
+    start_interface.start(command);
+
+    if (!start_interface.waitForFinished())
+        setError(QString("startInterface error: %1").arg(start_interface.errorString()));
+    start_interface.close();
+}
+
+void Network::stopInterface(QString interface)
+{
+    if (interface.isNull() || interface == "")
+    {
+        setError(QString("stopInterface error: Interface is null"));
+        return;
+    }
+    QString command = QString("/bin/bash \"ifdown %1\"").arg(interface);
+    QProcess start_interface;
+    start_interface.start(command);
+
+    if (!start_interface.waitForFinished())
+        setError(QString("stopInterface error: %1").arg(start_interface.errorString()));
+    start_interface.close();
 }
--- SystemSettings/network.h
+++ SystemSettings/network.h
@@ -15,13 +15,36 @@ public:
     explicit Network();
     ~Network();
 
+    /**
+     * @brief configure network settings
+     * @param QJsonObject data : data in JSON format
+     */
     bool setAddress(QJsonObject &data);
+    /**
+     * @brief writes the interfaces file with the stored data
+     */
+    bool writeInterfacesConf();
+    /**
+     * @brief starts the network interface
+     */
+    void startInterface(QString interface);
+    /**
+     * @brief stop network interface
+     */
+    void stopInterface(QString interface);
 
 signals:
 
 public slots:
 
 protected slots:
+
+protected:
+    /**
+     * @brief get interface settings list
+     * @param QStringList fields : list of field names of network table
+     * @param QString where : string with the where of query
+     */
     void getAddress(QStringList &fields, QString where);
 
 private:
@@ -29,6 +52,8 @@ private:
 
     //const QString m_interfaces_file = "/etc/network/interfaces";
     const QString m_interfaces_file = "/tmp/interfaces";
+    const QString m_content_interfaces_file = "# interfaces(5) file used by ifup(8) and ifdown(8)\n\nauto lo\niface lo inet loopback\n";
+    const QStringList m_default_interfaces = {"wlan0", "eth0"};
     const QString m_table = "Network";
     DatabaseSettings *db = new DatabaseSettings(m_table);
 
